5093206f355ebe6a7358e3aedab75158dc6e76f2
mukabyl <mukabyl@yamdex.ru>
2023-01-23 23:39:09 +0300

    Initial commit

---
+++ src/animal/Horse.java
@@ -0,0 +1,10 @@
+package animal;
+
+public class Hours extends Animal{
+    public Hours(String nickName, double weight, int age, String gender) {
+        super(nickName, weight, age, gender);
+    }
+
+    public Hours() {
+    }
+}
